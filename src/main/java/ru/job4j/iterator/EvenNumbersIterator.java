0aee7fa19f745a831bcfe5576cc2b31153bbc3f1
Evgen Kapaev <jeikhan@yandex.ru>
2021-10-17 15:18:07 +0300

    Итератор четные числа [#150]
    - упрощён метод hasNext() на определение отсутствующих значений.

--- src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -26,10 +26,10 @@ public class EvenNumbersIterator implements Iterator<Integer> {
      */
     @Override
     public boolean hasNext() {
-        while (index != data.length && data[index] % 2 != 0) {
+        while (index < data.length && data[index] % 2 != 0) {
             index++;
         }
-        return index != data.length && data[index] % 2 == 0;
+        return index < data.length && data[index] % 2 == 0;
     }
 
     /**
