4e6134ab54918601baa512fd26e49adc04de2d3f
Evgen Kapaev <jeikhan@yandex.ru>
2022-01-30 15:41:57 +0300

    Удалить head в односвязном списке. [#51424]

---
+++ src/main/java/ru/job4j/linkedlist/ForwardLinked.java
@@ -0,0 +1,94 @@
+package ru.job4j.linkedlist;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Задача: реализовать метод delete для односвязного списка.
+ *
+ * @author Evgenii Kapaev
+ * @since 30.01.22
+ */
+public class ForwardLinked<T> implements Iterable<T> {
+    private Node<T> head;
+
+    /**
+     * Вставка значения элемента.
+     *
+     * @param value значение элемента
+     */
+    public void add(T value) {
+        Node<T> node = new Node<>(value, null);
+        if (head == null) {
+            head = node;
+            return;
+        }
+        Node<T> tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    }
+
+    /**
+     * Удаление первого элемента из списка.
+     *
+     * @return удаляемый элемент.
+     */
+    public T deleteFirst() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        T tmp = head.value;
+        head = head.next;
+        return tmp;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            Node<T> node = head;
+
+            /**
+             * Проверка наличия последующего элемента.
+             *
+             * @return true or false.
+             */
+            @Override
+            public boolean hasNext() {
+                return node != null;
+            }
+
+            /**
+             * Считывание последующего элемента.
+             * Иначе генерируется исключение.
+             *
+             * @return последующий элемент.
+             */
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T value = node.value;
+                node = node.next;
+                return value;
+            }
+        };
+    }
+
+    /**
+     * Класс отдельного элемента связанного списка.
+     *
+     * @param <T> параметр типа элемента.
+     */
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
