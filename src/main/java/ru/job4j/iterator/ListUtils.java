6b6e1f918a97707bd5992151962f0047150374f4
Evgen Kapaev <jeikhan@yandex.ru>
2022-04-03 17:21:29 +0300

    ListIterator [#350217]
    - упрощен метод addAfter().

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -40,14 +40,8 @@ public class ListUtils {
      */
     public static <T> void addAfter(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> iterator = list.listIterator(index);
-        while (iterator.hasNext()) {
-            if (iterator.nextIndex() == index) {
-                list.add(value);
-                break;
-            }
-            iterator.next();
-        }
+        ListIterator<T> iterator = list.listIterator(index + 1);
+        iterator.add(value);
     }
 
     /**
