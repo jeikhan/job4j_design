d72ded3d10675d609a542a8844ecc671758feb8c
Evgen Kapaev <jeikhan@yandex.ru>
2022-04-13 12:32:06 +0300

    Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Создать модель User и добавить три поля:
@@ -10,9 +13,9 @@ import java.util.Calendar;
  * @since 08.04.2022
  */
 public class User {
-    private String name;
-    private int children;
-    private Calendar birthday;
+    private final String name;
+    private final int children;
+    private final Calendar birthday;
 
     public User(String name, int children, Calendar birthday) {
         this.name = name;
@@ -20,5 +23,25 @@ public class User {
         this.birthday = birthday;
     }
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", children=" + children +
+                ", birthday=" + birthday.getWeekYear() +
+                '}';
+    }
 
+    public static void main(String[] args) {
+        User user1 = new User("Иванов Иван", 2, new GregorianCalendar(1960, Calendar.APRIL, 11));
+        User user2 = new User("Иванов Иван", 2, new GregorianCalendar(1960, Calendar.APRIL, 11));
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        map.put(user2, new Object());
+        for (User element : map.keySet()) {
+            System.out.println(element);
+        }
+    }
 }
