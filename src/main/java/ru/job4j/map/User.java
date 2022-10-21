ccbd9ec965e92b7697f5f6cb89f90bf1cafa6431
Evgen Kapaev <jeikhan@yandex.ru>
2022-10-21 14:03:01 +0300

    Исправления
    - переопределение hashCode(), equals().

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,8 +1,6 @@
 package ru.job4j.map;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
+import java.util.*;
 import java.util.Map;
 
 /**
@@ -24,6 +22,23 @@ public class User {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof User)) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
+    @Override
     public String toString() {
         return "User{"
                 + "name='" + name + '\''
@@ -36,12 +51,18 @@ public class User {
         User user1 = new User("Иванов Иван", 2, new GregorianCalendar(1960, Calendar.APRIL, 11));
         User user2 = new User("Иванов Иван", 2, new GregorianCalendar(1960, Calendar.APRIL, 11));
         Map<User, Object> map = new HashMap<>();
-        map.put(user1, new Object());
+        System.out.println("Хеш-коды объектов:");
+        System.out.println("user1: " + user1.hashCode());
+        System.out.println("user2: " + user2.hashCode());
         map.put(user1, new Object());
         map.put(user2, new Object());
-        map.put(user2, new Object());
+        System.out.println("Значения объектов:");
+        System.out.println("user1: " + map.get(user1));
+        System.out.println("user2: " + map.get(user2));
+        System.out.println("Вывод:");
         for (User element : map.keySet()) {
-            System.out.println(element);
+            System.out.print(element + " " + "hashCode = ");
+            System.out.println(element.hashCode());
         }
     }
 }
