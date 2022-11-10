fc9c633cacce3625fc399e1ea1d591d3f3075910
Evgen Kapaev <jeikhan@yandex.ru>
2022-11-10 16:55:40 +0300

    Тестирование IO [#173905]

---
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.*;
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Интеграционные тесты на метод unavailable() класса Analizy.
+ */
+class AnalizyTest {
+    @Test
+    public void whenUnavailable(@TempDir Path tempDir) throws IOException {
+        File source = tempDir.resolve("server.log").toFile();
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
+        }
+        File target = tempDir.resolve("unavailable.csv").toFile();
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder result = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(result::append);
+        }
+        assertThat("10:57:01 - 10:59:01;11:01:02 - 11:02:02;").isEqualTo(result.toString());
+    }
+}
